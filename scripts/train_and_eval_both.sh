3438bacd011ebc4beb2fe6e92993b7c291c2d909
peter <you@example.com>
2026-01-09 14:54:09 -0500

    added extra training scripts and changed to eval on proiel trees

--- scripts/train_and_eval_both.sh
+++ scripts/train_and_eval_both.sh
@@ -40,4 +40,18 @@ source activate general_purpose_textgen
 echo "Starting post-training with WSD"
 cd ../wsd
 python wsd.py
+echo "Post-training with WSD finished"
+
+echo "Post-training with spaCy"
+source deactivate
+
+echo "Activating env"
+source activate spacy_gpu
+
+echo "Starting post-training with spaCy"
+cd ../spacy
+spacy train configs/gpu_default.cfg --paths.train corpus/train.spacy --paths.dev corpus/dev.spacy --gpu-id 0 --output ./output
 echo "Post-training finished"
+echo "Evaluating spaCy model"
+spacy evaluate ./output/model-best corpus/test.spacy --gpu-id 0 --output ./output/eval_results.json
+echo "spaCy Evaluation finished"
